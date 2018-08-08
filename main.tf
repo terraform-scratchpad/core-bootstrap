1928f3515f96f0d7f90aebb122c4144050a04b73
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-08 11:17:40 +0200

    ARM_ACCESS_KEY set locally after template execution is done

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   version = "1.12.0"
 }
 
-variable resource_group_name {
+variable tf_state_resource_group_name {
   type = "string"
   default = "QA-DIOR-TF-STATE"
 }
@@ -23,7 +23,7 @@ variable "tags" {
 
 resource "azurerm_resource_group" "tf-state-rg" {
   location                  = "${var.location}"
-  name                      = "${var.resource_group_name}"
+  name                      = "${var.tf_state_resource_group_name}"
   tags                      = "${var.tags}"
 }
 
@@ -40,4 +40,14 @@ resource "azurerm_storage_container" "tf-state-container" {
   name                      = "qatfstatecnt"
   resource_group_name       = "${azurerm_resource_group.tf-state-rg.name}"
   storage_account_name      = "${azurerm_storage_account.tf-state-storage-account.name}"
+}
+
+resource "null_resource" "set-access-key" {
+  provisioner "local-exec" {
+    command = <<EOT
+echo "ARM_ACCESS_KEY=${azurerm_storage_account.tf-state-storage-account.primary_access_key}" > /usr/local/var/azure-terraform-access_key.sh
+source /usr/local/var/azure-terraform-access_key.sh
+echo "env var ARM_ACCESS_KEY has been set locally"
+    EOT
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,3 @@
-output "tfstate-access-key" {
-  value = "${azurerm_storage_account.tf-state-storage-account.primary_access_key}"
-}
 output "tfstate-container-name" {
   value = "${azurerm_storage_container.tf-state-container.name}"
 }
