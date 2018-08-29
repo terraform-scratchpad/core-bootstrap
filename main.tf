61d100222581c5f73cd6a1c54bfd111e869cad4e
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-29 16:26:15 +0200

    renamed location/filename containing storage access_key

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 #
-# Terraform template creates storage account and container serving as backend for
-# all QAs configurations
+# bootstrap Terraform template creates the following essential shared resources
+# main resource group
+# storage account and container serving as backend (stores tfstate files for the upcoming infrastructure states)
 #
 provider "azurerm" {
-  version = "1.12.0"
+  version = "~> 1.12.0"
 }
 
 resource "azurerm_resource_group" "tf-state-rg" {
@@ -27,11 +28,14 @@ resource "azurerm_storage_container" "tf-state-container" {
   storage_account_name      = "${azurerm_storage_account.tf-state-storage-account.name}"
 }
 
+#
+# puts generated storage account access_key into a file on the host running this template
+#
 resource "null_resource" "storage-container-access-key" {
   provisioner "local-exec" {
     command = <<EOT
-echo "access_key = ${azurerm_storage_account.tf-state-storage-account.primary_access_key}" > /usr/local/var/azure-tfstate-backend-access_key.cf
-echo "terraform backend container ${azurerm_storage_container.tf-state-container.name} with new access_key stored at /usr/local/var/azure-tfstate-backend-access_key.cf"
+echo "access_key=${azurerm_storage_account.tf-state-storage-account.primary_access_key}" > ~/.secrets/backend-access_key.cf
+echo "terraform backend container ${azurerm_storage_container.tf-state-container.name} with new access_key stored at ~/.secrets/backend-access_key.cf"
     EOT
   }
 }
